7836b43ffeaf75aa5472405fa934af73d28e3c2c
alphiii <blazbider@gmail.com>
2017-06-12 13:09:31 +0200

    Fixed refreshing screen, working on Following devices

--- sniffer.py
+++ sniffer.py
@@ -1,12 +1,8 @@
-import datetime
 import sys
 import time
 import logging
-from threading import Thread
 
-import serial
 from apscheduler.schedulers.background import BackgroundScheduler
-
 from asciimatics.exceptions import NextScene, StopApplication, ResizeScreenError
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
@@ -19,7 +15,7 @@ from SnifferAPI import Sniffer
 
 from Project import config
 from Project.client import Client
-from Project.shared import get_Address, ReloadSniffer
+from Project.shared import get_Address
 from Project.shared import CloseSnifferException
 from Project.logging_packets import initialize_packets_logging_to_Filebeat
 from Project.logging_service import initialize_service_logging
@@ -29,7 +25,7 @@ client = None
 logger = logging.getLogger(config.SERVICE_LOGGER)
 
 
-class MainView(Frame, Thread):
+class MainView(Frame):
     def __init__(self, screen, client):
         super(MainView, self).__init__(screen,
                                        screen.height * 2 // 3,
@@ -38,16 +34,13 @@ class MainView(Frame, Thread):
                                        hover_focus=True,
                                        title="Bluetooth Low Energy Sniffer",
                                        reduce_cpu=True)
-        Thread.__init__(self)
-        # Save off the model that accesses the contacts database.
+
         self._client = client
         self._screen = screen
         self._frame_num = 0
         self._devices = []
-        self.daemon = True
-        self.name = "scanning"
 
-        # Create the form for displaying the list of contacts.
+        # Create the form for displaying the list of found devices.
         self._list_view = ListBox(
             Widget.FILL_FRAME,
             name="devices",
@@ -55,6 +48,7 @@ class MainView(Frame, Thread):
             on_change=self._on_pick
         )
 
+        # Create the form for displaying the list of client information.
         self._client_info_view = MultiColumnListBox(
             Widget.FILL_FRAME,
             columns=["<50%", "<50%"],
@@ -78,21 +72,22 @@ class MainView(Frame, Thread):
         self._button_layout.add_widget(self._follow_button, 0)
         self._button_layout.add_widget(Button("Quit", self._quit), 1)
 
-
-        self.start()
+        global  followed_device
+        followed_device = None
 
         self.fix()
         self._on_pick()
 
-        #self.sched = BackgroundScheduler(daemon=True)
-        #self.sched.start()
-        #self.sched.add_job(self.run, 'interval', seconds=config.UPDATE_SCREEN_INTERVAL, max_instances=1)
+        self.sched = BackgroundScheduler(daemon=True)
+        self.sched.start()
+        self.sched.add_job(self.run, 'interval', seconds=config.UPDATE_SCREEN_INTERVAL, max_instances=1, id="scanning")
 
     def _on_pick(self):
         self._follow_button.disabled = self._list_view.value is None
 
     def _follow(self):
-        self.save()
+        global followed_device
+        followed_device = self.data["devices"]
         raise NextScene("Follow Device")
 
     def _get_device_info(self):
@@ -124,27 +119,27 @@ class MainView(Frame, Thread):
     def run(self):
         global mySniffer
         logger = logging.getLogger(config.SERVICE_LOGGER)
-        while 1:
-            starttime = time.time()
-            try:
-                if mySniffer is None or self._client.port is None:
-                    setup(6)
+        starttime = time.time()
+        try:
+            if mySniffer is None or self._client.port is None:
+                setup(6)
 
-                mySniffer.scan()
-                time.sleep(config.UPDATE_SCREEN_INTERVAL - ((time.time() - starttime) % config.UPDATE_SCREEN_INTERVAL))
-                self._devices = mySniffer.getDevices().asList()
-                self.update_client_info()
-                self.reload_devices()
-                self._info_layout.update_widgets()
-                self._screen.force_update()
-            except Exception as e:
-                time.sleep(5)
-                logger.exception("Background Service Exception", exc_info=True)
-                if mySniffer: mySniffer.doExit()
-                time.sleep(11)
-                self._client = Client()
-                mySniffer = None
-
+            mySniffer.scan()
+            time.sleep(config.UPDATE_SCREEN_INTERVAL - ((time.time() - starttime) % config.UPDATE_SCREEN_INTERVAL))
+            self._devices = mySniffer.getDevices().asList()
+            self._client.update_client_with_sniffer(mySniffer)
+            self.update_client_info()
+            self.reload_devices()
+            self._info_layout.update_widgets()
+            self._screen.force_update()
+        except Exception as e:
+            #Closing ser port is already done in Sniffer API
+            time.sleep(5)
+            logger.exception("Background Service Exception", exc_info=True)
+            self._client = Client()
+            self.update_client_info()
+            self._list_view.options = []
+            mySniffer = None
 
     def _quit(self):
         self._scene.add_effect(
@@ -170,27 +165,53 @@ class FollowView(Frame):
 
         self._device = device
         self._client = client
+        self._packets = []
 
-        self._client_info_view = MultiColumnListBox(
+        # Create the form for displaying the list of sniffed packets.
+        self._list_view = ListBox(
             Widget.FILL_FRAME,
-            columns=[">50%"],
-            label="Client Information",
-            name="client_info_view",
-            options=self._get_client_info(),
-            on_change=self._get_client_info()
+            name="packets",
+            options=self._get_packets_info(),
         )
 
-        layout = Layout([100], fill_frame=True)
-        self.add_layout(layout)
-        layout.add_widget(Divider())
+        # Create the form for displaying the list of client information.
+        self._client_info_view = MultiColumnListBox(
+            Widget.FILL_FRAME,
+            columns=["<50%", "<50%"],
+            label=None,
+            name="client_info_view",
+            options=self._get_client_info(),
+        )
+
+        self._info_layout = Layout([100, 100], fill_frame=True)
+        self.add_layout(self._info_layout)
+        self._info_layout.add_widget(self._client_info_view, column=0)
+        self._info_layout.add_widget(self._list_view, column=1)
+
+        self._divider_layout = Layout([1])
+        self.add_layout(self._divider_layout)
+        self._divider_layout.add_widget(Divider())
+
         layout2 = Layout([1, 1])
         self.add_layout(layout2)
         layout2.add_widget(Button("Quit", self._quit_confirm), 0)
-        layout2.add_widget(Button("Back", self._quit_confirm), 1)
+        layout2.add_widget(Button("Back", self._back), 1)
         self.fix()
 
     def _back(self):
         raise NextScene("Main")
+
+    def _get_packets_info(self):
+        list_of_packets = []
+        count = 0
+        if self._packets:
+            for packet in self._packets:
+                count += 1
+                list_of_packets.append(("RSSI: {} dBm | Payload: {}".format(packet.RSSI,
+                    packet.blePacket.payload if packet.blePacket else packet), count))
+        else:
+            list_of_packets = []
+        return list_of_packets
 
     def _get_client_info(self):
         client_options = []
@@ -200,10 +221,37 @@ class FollowView(Frame):
             client_options.append(([client_info_key, client_info_value], items))
         return client_options
 
+    def run(self):
+        global mySniffer
+        logger = logging.getLogger(config.SERVICE_LOGGER)
+        starttime = time.time()
+        try:
+            if mySniffer is None or self._client.port is None or self._device is None:
+                raise NextScene("Main")
+
+            mySniffer.follow(self._device)
+            self._packets = mySniffer.getPackets()
+            time.sleep(config.UPDATE_SCREEN_INTERVAL - ((time.time() - starttime) % config.UPDATE_SCREEN_INTERVAL))
+            self._client.update_client_with_sniffer(mySniffer)
+            self.update_client_info()
+            self._get_packets_info()
+            self._info_layout.update_widgets()
+            self._screen.force_update()
+        except Exception as e:
+            #Closing ser port is already done in Sniffer API
+            time.sleep(5)
+            logger.exception("Background Service Exception", exc_info=True)
+            self._client = Client()
+            self.update_client_info()
+            self._list_view.options = []
+            mySniffer = None
+
+    def update_client_info(self):
+        self._client_info_view.options = self._get_client_info()
+
     @staticmethod
     def _quit_confirm():
         raise StopApplication("User pressed quit")
-
 
 
 def setup(delay=6):
@@ -221,12 +269,10 @@ def setup(delay=6):
         initialize_packets_logging_to_Filebeat()
         logger.info("Capturing data to Filebeat")
 
-    # Try to open the serial port, here we start logging
-
     # Initialize the device without specified serial port
+    # TODO: This is hack, should have proper automatic port discovery
     for port in list_ports.grep(config.SNIFFER_PORT_KEYWORD_SEARCH):
         try:
-            #TODO: This is hack, should have proper automatic port discovery
             mySniffer = Sniffer.Sniffer(port.device)
             mySniffer.start()
             time.sleep(delay)
@@ -244,26 +290,28 @@ def setup(delay=6):
             break
 
 last_scene = None
+followed_device = None
 
 def demo(screen, scene):
-    global client
+    global client, followed_device
+
     main_scene = MainView(screen, client)
     scenes = [
         Scene([main_scene], -1, name="Main"),
-        Scene([FollowView(screen, client, None)], -1, name="Follow Device")
+        Scene([FollowView(screen, client, followed_device)], -1, name="Follow Device")
     ]
 
-    screen.set_scenes(scenes)
+    screen.set_scenes(scenes, start_scene=scene)
 
     while True:
         screen.draw_next_frame(repeat=True)
         time.sleep(0.05)
 
-
-    #screen.play(scenes, stop_on_resize=True, start_scene=scene)
-
-
-
+        #TODO: Screen resizing
+        #if screen.has_resized():
+            #screen._scenes[screen._scene_index].exit()
+            #raise ResizeScreenError("Screen resized",
+            #                    screen._scenes[screen._scene_index])
 
 
 def main():
@@ -274,13 +322,12 @@ def main():
             Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
             if mySniffer: mySniffer.doExit()
             sys.exit(-1)
-        except ResizeScreenError as e:
-            last_scene = e.scene
-        except ReloadSniffer:
-            logger.warning("Reloading Sniffer")
-            if mySniffer: mySniffer.doExit()
         except CloseSnifferException:
-            logger.info("Service has been closed (CloseSnifferException)")
+            logger.exception("Closing Sniffer", exc_info=True)
+            if mySniffer: mySniffer.doExit()
+            sys.exit(-1)
+        except StopApplication:
+            logger.info("Application Exit (user pressed Quit)")
             if mySniffer: mySniffer.doExit()
             sys.exit(-1)
         except Exception as e:
