2f04a2581b90e31f29bb1088173deef72094795f
alphiii <blazbider@gmail.com>
2017-06-13 15:40:18 +0200

    Fixed logging for RPi

--- Project/logging_service.py
+++ Project/logging_service.py
@@ -1,6 +1,5 @@
 import os
 import logging
-from functools import reduce
 
 from pythonjsonlogger import jsonlogger
 
@@ -16,12 +15,25 @@ class ServiceFilter(logging.Filter):
         record.client = self.client.to_JSON()
         return True
 
+
+def initialize_packets_logging_to_Filebeat():
+    log_file = os.path.join("Logs", PACKETS_LOG_FILE_NAME)
+    logger = logging.getLogger(BLE_PACKETS_LOGGER)
+    logger.setLevel(PACKETS_LOG_LEVEL)
+    logger.propagate = False
+    handler = logging.handlers.TimedRotatingFileHandler(log_file,
+                                       when=PACKETS_LOG_FILE_TIME_BASE,
+                                       interval=1,
+                                       backupCount=PACKETS_CLEAN_UP_INTERVAL,
+                                       utc=True)
+    logger.addHandler(handler)
+
 def initialize_service_logging(client):
     log_file = os.path.join("Logs", SERVICE_LOG_FILE_NAME)
     logger = logging.getLogger(SERVICE_LOGGER)
     logger.addFilter(ServiceFilter(client))
-    logger.propagate = False
     logger.setLevel(SERVICE_LOG_LEVEL)
+    logger.propagate = False
     handler = logging.handlers.TimedRotatingFileHandler(log_file,
                                        when=SERVICE_LOG_FILE_TIME_BASE,
                                        interval=1,
@@ -35,6 +47,7 @@ def initialize_service_logging(client):
 def initialize_scheduler_logging():
     log_file = os.path.join("Logs", SCHEDULER_LOG_FILE_NAME)
     logger = logging.getLogger(SCHEDULER_LOGGER)
+    logger.setLevel(SCHEDULER_LOG_LEVEL)
     logger.propagate = False
     handler = logging.handlers.TimedRotatingFileHandler(log_file,
                                                         when=SCHEDULER_LOG_FILE_TIME_BASE,
