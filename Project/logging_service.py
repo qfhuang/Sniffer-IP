31848880732e9d1890efe68ffa3b2ae748f977f2
alphiii <blazbider@gmail.com>
2017-06-14 15:49:48 +0200

    RPi stoud logg cancel

--- Project/logging_service.py
+++ Project/logging_service.py
@@ -20,6 +20,7 @@ def initialize_packets_logging_to_Filebeat():
     log_file = os.path.join("Logs", PACKETS_LOG_FILE_NAME)
     logger = logging.getLogger(BLE_PACKETS_LOGGER)
     logger.setLevel(PACKETS_LOG_LEVEL)
+    stream_handler = logging.StreamHandler(stream=None)
     logger.propagate = False
     handler = logging.handlers.TimedRotatingFileHandler(log_file,
                                        when=PACKETS_LOG_FILE_TIME_BASE,
@@ -27,6 +28,7 @@ def initialize_packets_logging_to_Filebeat():
                                        backupCount=PACKETS_CLEAN_UP_INTERVAL,
                                        utc=True)
     logger.addHandler(handler)
+    logger.addHandler(stream_handler)
 
 def initialize_service_logging(client):
     log_file = os.path.join("Logs", SERVICE_LOG_FILE_NAME)
@@ -34,6 +36,7 @@ def initialize_service_logging(client):
     logger.addFilter(ServiceFilter(client))
     logger.setLevel(SERVICE_LOG_LEVEL)
     logger.propagate = False
+    stream_handler = logging.StreamHandler(stream=None)
     handler = logging.handlers.TimedRotatingFileHandler(log_file,
                                        when=SERVICE_LOG_FILE_TIME_BASE,
                                        interval=1,
@@ -42,13 +45,16 @@ def initialize_service_logging(client):
     format_str = '%(message)%(levelname)%(name)%(asctime)%(client)'
     formatter = jsonlogger.JsonFormatter(format_str, '%Y-%m-%dT%H:%M:%S')
     handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
     logger.addHandler(handler)
+
 
 def initialize_scheduler_logging():
     log_file = os.path.join("Logs", SCHEDULER_LOG_FILE_NAME)
     logger = logging.getLogger(SCHEDULER_LOGGER)
     logger.setLevel(SCHEDULER_LOG_LEVEL)
     logger.propagate = False
+    stream_handler = logging.StreamHandler(stream=None)
     handler = logging.handlers.TimedRotatingFileHandler(log_file,
                                                         when=SCHEDULER_LOG_FILE_TIME_BASE,
                                                         interval=1,
@@ -57,4 +63,5 @@ def initialize_scheduler_logging():
     format_str = '%(message)%(levelname)%(name)%(asctime)'
     formatter = jsonlogger.JsonFormatter(format_str, '%Y-%m-%dT%H:%M:%S')
     handler.setFormatter(formatter)
+    logger.addFilter(stream_handler)
     logger.addHandler(handler)
